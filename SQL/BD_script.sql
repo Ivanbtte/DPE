ec1a0f819385629ed46ad7396e8e1caf5955b3a1
Ivanbtte <92815595+Ivanbtte@users.noreply.github.com>
2022-04-06 10:28:39 -0500

    Update BD_script.sql

--- SQL/BD_script.sql
+++ SQL/BD_script.sql
@@ -1,20 +1,21 @@
 Create database DPE;
 
 create table Productos(
-	codigo serial,
+	codigo serial primary key,
 	nombre varchar(20),
-	descripcion(50),
-	precio(50),
+	descripcion varchar(50),
+	precio double precision,
 	fecha_creacion date,
 	fecha_actualizacion date,
 	fecha_eliminacion date
 );
 
 create table usuarios(
-	codigo serial,
+	codigo serial primary key,
 	nombre_usuario varchar(20),
 	contraseña varchar(20),
 	nombre varchar(50),
 	sexo varchar(20),
 	edad integer
 );
+
